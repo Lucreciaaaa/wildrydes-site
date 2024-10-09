16a85e85a83f54e0196f165d749bec1123ca0db4
Lucreciaaaa <lfodouop@gmail.com>
2024-10-09 09:12:49 +0200

    new config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_9JsbkaW4R', // id du groupe utilisateur
+        userPoolClientId: '4m7s9etjsfg4tihk858q9jbqk5', // id du client applicateur
+        region: 'eu-north-1' 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
